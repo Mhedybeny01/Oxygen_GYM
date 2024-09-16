ba5a0f67692971b3cfdbb96112b501857b362936
Mhedybeny01 <mehdibentaleb548@gmail.com>
2024-09-16 23:19:07 +0100

    Add files via upload

---
+++ web.js
@@ -0,0 +1,14 @@
+const content = document.getElementById("hoverbakh");
+
+    function display(){
+        content.style.right = "780px";
+        content.style.left = "auto";  
+    } 
+    function closs(){
+        content.style.left = "-50%";
+    }
+
+
+
+    
+
